ac5d92135811b5b584e886f7d2741c20ec2c24b1
samar benmouelli <benmouellisamar@gmail.com>
2023-10-08 07:07:16 -0700

    Add MIT License

---
+++ app/Models/Produit.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Produit extends Model
+{
+    use HasFactory;
+    protected $fillable = [
+        'facebook_post_link','code','current_quantity','title','price','number_purchases'
+        ];
+
+}
